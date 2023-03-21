27c96acc6f7681479e87c259372577454150d415
Bruno Mera <thenunix@gmail.com>
2023-03-21 18:57:57 -0300

    test

---
+++ conf.lua
@@ -0,0 +1,4 @@
+function love.conf(t)
+    t.window.title = "Test"
+    t.window.icon = "icon.png"
+end
---
+++ main.lua
@@ -0,0 +1,4 @@
+--this is a test
+function love.draw()
+    love.graphics.print("Hello")
+end
